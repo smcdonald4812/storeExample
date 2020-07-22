315b70281eaf0b2b717b04e2945720a126e6fa9a
smcdonald4812 <36778056+smcdonald4812@users.noreply.github.com>
2020-07-22 13:12:26 -0400

    Add files via upload
    base for rabbitMQ queueing service for store example

---
+++ microservices/queueingService/src/main/java/com/mcdonald/QueueingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mcdonald;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueingServiceApplication.class, args);
+	}
+
+}
---
+++ microservices/queueingService/src/test/java/com/mcdonald/QueueingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcdonald;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
