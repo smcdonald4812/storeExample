c428796bd32680e03642f95986938eda3a020f4e
smcdonald4812 <36778056+smcdonald4812@users.noreply.github.com>
2020-07-27 15:07:45 -0400

    Add files via upload
    started to create pagination and item cards on home page

--- reactFrontEnd/src/pages/home.js
+++ reactFrontEnd/src/pages/home.js
@@ -5,24 +5,31 @@ import {Link} from 'react-router-dom';
 
 class Home extends React.Component {
 
+    //going to use pagination to load in 12 item chunks
     loadChoices() {
-                var rows = [];
-                for (let index = 0; index < 1; index++) {
-                    rows.push(
-                        <div key="index">
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src="holder.js/100px180" />
-                                <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                </Card.Text>
-                                <Button variant="primary" as={Link} to="/item">Check it out</Button>
-                                </Card.Body>
-                            </Card>
-                        </div>
+                var rows = [], columns = [];
+                for (let index = 1; index < 12; index++) {
+                    columns.push(
+                        <td key={index}>
+                                <Card style={{ width: '18rem' }}>
+                                    <Card.Img variant="top" src="holder.js/100px180" />
+                                    <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of
+                                        the card's content.
+                                    </Card.Text>
+                                    <Button variant="primary" as={Link} to="/item" key={index}>Check it out</Button>
+                                    </Card.Body>
+                                </Card>
+                            </td>
                     )
+                    if(index%3==0) {
+                        rows.push(
+                            <tr>{columns}</tr>
+                        )
+                        columns=[];
+                    }
                 }
             return <tbody>{rows}</tbody>;
     }
@@ -31,7 +38,7 @@ class Home extends React.Component {
         return (
             <div className="mr-auto" style={{ marginTop: "100px"}}>
                 <div className="mr-auto">
-                    <h1>Home page</h1>
+                    <h1 className="mr-auto">Home page</h1>
                     <table className="table table-borderless">
                         {this.loadChoices()}
                     </table>
